38306467466f3c5ce5e3a284cd3b88f68697f5a2
vtta <vtta0124@gmail.com>
2020-02-25 19:53:13 +0800

    Running the kernel with qemu

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #![no_std]
 #![no_main]
+#![feature(asm)]
 #![feature(global_asm)]
 
 use core::panic::PanicInfo;
@@ -12,6 +13,25 @@ fn panic(_: &PanicInfo) -> ! {
 }
 
 #[no_mangle]
-pub extern "C" fn rust_main() -> ! {
+extern "C" fn rust_main() -> ! {
+    console_putchar(b'O');
+    console_putchar(b'K');
+    console_putchar(b'\n');
     loop {}
+}
+
+pub fn console_putchar(ch: u8) {
+    let ret: usize;
+    let arg0: usize = ch as usize;
+    let arg1: usize = 0;
+    let arg2: usize = 0;
+    let which: usize = 1;
+    unsafe {
+        asm!("ecall"
+             : "={x10}" (ret)
+             : "{x10}" (arg0), "{x11}" (arg1), "{x12}" (arg2), "{x17}" (which)
+             : "memory"
+             : "volatile"
+        );
+    }
 }
