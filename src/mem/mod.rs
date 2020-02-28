f7ee1b8ddc0658e29ff8c85f23774c85c2a72807
vtta <vtta0124@gmail.com>
2020-02-28 12:33:49 +0800

    Test heap init before physical memory allocator init

--- src/mem/mod.rs
+++ src/mem/mod.rs
@@ -10,10 +10,10 @@ pub fn init() {
     extern "C" {
         fn end();
     }
-    let bppn = ((end as usize - KERNEL_BEGIN_VADDR + KERNEL_BEGIN_PADDR) / PAGE_SIZE + 1).into();
-    let eppn = (PHYSICAL_MEMORY_END / PAGE_SIZE).into();
-    frame::init(bppn, eppn);
-    frame::test(bppn, eppn);
     heap::init();
     heap::test();
+    let bppn = (end as usize - KERNEL_BEGIN_VADDR + KERNEL_BEGIN_PADDR) / PAGE_SIZE + 1;
+    let eppn = PHYSICAL_MEMORY_END / PAGE_SIZE;
+    frame::init(bppn, eppn);
+    frame::test(bppn, eppn);
 }
